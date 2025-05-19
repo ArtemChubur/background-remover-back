61046d4e9acc2a578ac10b5b554c182e8553c726
ArtemChubur <artemchubur@gmail.com>
2025-05-20 00:11:58 +0600

    new model

--- app.py
+++ app.py
@@ -15,6 +15,43 @@
 
 
 
+# from flask import Flask, request, send_file, jsonify
+# from flask_cors import CORS
+# from rembg import remove
+# from PIL import Image
+# import io
+
+# app = Flask(__name__)
+# CORS(app, origins=["http://localhost:5173"])  # Разрешить запросы с Vite
+
+# @app.route('/')
+# def index():
+#     return 'Background Removal API is running!'
+
+# @app.route('/remove-bg', methods=['POST'])
+# def remove_bg():
+#     if 'image' not in request.files:
+#         return jsonify({'error': 'No image provided'}), 400
+
+#     image_file = request.files['image']
+#     input_image = Image.open(image_file.stream).convert("RGBA")
+    
+#     # Remove background
+#     output_image = remove(input_image)
+
+#     # Save to in-memory file
+#     byte_io = io.BytesIO()
+#     output_image.save(byte_io, 'PNG')
+#     byte_io.seek(0)
+
+#     return send_file(byte_io, mimetype='image/png')
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+
 from flask import Flask, request, send_file, jsonify
 from flask_cors import CORS
 from rembg import remove
@@ -36,10 +73,10 @@ def remove_bg():
     image_file = request.files['image']
     input_image = Image.open(image_file.stream).convert("RGBA")
     
-    # Remove background
-    output_image = remove(input_image)
+    # Убираем фон с использованием модели isnet-general-use
+    output_image = remove(input_image, model_name='isnet-general-use')
 
-    # Save to in-memory file
+    # Сохраняем результат в памяти
     byte_io = io.BytesIO()
     output_image.save(byte_io, 'PNG')
     byte_io.seek(0)
